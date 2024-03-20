79da39dd4436d253bfeb52117cb6d4d5173787a4
ricardoq4p <151384080+ricardoq4p@users.noreply.github.com>
2024-03-20 12:55:30 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Sempre que houver alternativas, tenha cuidado. Não opte pelo conveniente, pelo confortável, pelo respeitável, pelo socialmente aceitável, pelo honroso. Opte pelo que faz o seu coração vibrar. Opte pelo que gostaria de fazer, apesar de todas as consequências. -Osho</h1>
+    return <h1>Sempre que houver alternativas, tenha cuidado. Não opte pelo conveniente, pelo confortável, pelo respeitável, pelo socialmente aceitável, pelo honroso. Opte pelo que faz o seu coração vibrar. Opte pelo que gostaria de fazer, apesar de todas as consequências. -Osho!</h1>
 }
 
 export default Home;
