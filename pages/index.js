3a035d75109f2878e85649df0e06083dd1dedce9
ricardoq4p <151384080+ricardoq4p@users.noreply.github.com>
2024-03-21 18:00:29 +0000

    Aplica correções de formatação com Prettier e trata vunerabilidades

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Home() {
   const [mostrarEmoticon, setMostrarEmoticon] = useState(false);
 
   return (
-    <div style={{ backgroundColor: 'hsl(180, 100%, 75%)', minHeight: '100vh' }}>
-      <h1>Sempre que houver alternativas, tenha cuidado. Não opte pelo conveniente, pelo confortável, pelo respeitável, pelo socialmente aceitável, pelo honroso. Opte pelo que faz o seu coração vibrar. Opte pelo que gostaria de fazer, apesar de todas as consequências. -Osho!</h1>
+    <div style={{ backgroundColor: "hsl(180, 100%, 75%)", minHeight: "100vh" }}>
+      <h1>
+        Sempre que houver alternativas, tenha cuidado. Não opte pelo
+        conveniente, pelo confortável, pelo respeitável, pelo socialmente
+        aceitável, pelo honroso. Opte pelo que faz o seu coração vibrar. Opte
+        pelo que gostaria de fazer, apesar de todas as consequências. -Osho!
+      </h1>
       <button onClick={() => setMostrarEmoticon(true)}>Enviar Beijo</button>
       {mostrarEmoticon && <p>:D</p>}
     </div>
