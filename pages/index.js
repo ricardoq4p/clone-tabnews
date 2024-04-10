196e3051fdbeeb002850c7cd6abca457e1005839
ricardoq4p <151384080+ricardoq4p@users.noreply.github.com>
2024-04-10 00:55:46 +0000

    site em contrução

--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ const HomePage = () => {
       >
         <p>SITE EM CONSTRUÇÃO!</p>
         <img
-          src="https://github.com/ricardoq4p/clone-tabnews/blob/main/caminho/para/imagem/construcao.png?raw=true"
+          src="https://github.com/ricardoq4p/clone-tabnews/blob/main/construcao.png?raw=true"
           alt="Em Construção"
           style={{ maxWidth: "100%", height: "auto" }}
         />
