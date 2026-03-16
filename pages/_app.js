c164861661aa921e2cccfdd6c947d93102c276b0
ricardoq4p <151384080+ricardoq4p@users.noreply.github.com>
2026-03-16 04:43:39 +0000

    corrige _app.js removendo do terminal

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
-# Criar o arquivo _app.js na pasta pages
-cat > pages/_app.js << 'EOF'
-import React from 'react';
+import React from "react";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
 export default MyApp;
-EOF
-
-# Verificar se criou
-ls -la pages/
