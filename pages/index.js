d5847cc3cd34cfd5dffb2049b8709dbb8928c66d
ricardoq4p <151384080+ricardoq4p@users.noreply.github.com>
2026-03-18 05:49:02 +0000

    feat> redireciona raiz para pagina de login

--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,16 @@
-import React from "react";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-const HomePage = () => {
-  // Aqui você pode adicionar lógica JavaScript adicional, como estados e efeitos, se necessário.
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  }, []);
 
   return (
-    <div
-      style={{
-        fontFamily: "Arial, sans-serif",
-        backgroundColor: "rgb(227, 227, 238)",
-        color: "white",
-        fontSize: "20pt",
-      }}
-    >
-      <header
-        style={{
-          textAlign: "center",
-          padding: "20px",
-          borderBottom: "2px solid #000",
-        }}
-      >
-        <img
-          src="https://github.com/ricardoq4p/clone-tabnews/blob/main/PanteraLab.png?raw=true"
-          alt="Logo Pantera Lab"
-          style={{ height: "100px" }}
-        />
-        <h1 style={{ color: "rgb(45, 75, 134)" }}>Seu futuro mini-fórum!</h1>
-      </header>
-
-      <main
-        style={{
-          textAlign: "center",
-          padding: "20px",
-          borderBottom: "2px solid #000",
-          minHeight: "400px",
-          color: "rgb(45, 75, 134)",
-        }}
-      >
-        <p>SITE EM CONSTRUÇÃO!</p>
-        <img
-          src="https://github.com/ricardoq4p/clone-tabnews/blob/main/contrucao.png?raw=true"
-          alt="Em Construção"
-          style={{ maxWidth: "100%", height: "auto" }}
-        />
-        {/* Para interações dinâmicas, considere usar estado e outros hooks do React */}
-      </main>
-
-      <footer
-        style={{
-          textAlign: "center",
-          padding: "20px",
-          borderTop: "2px solid #000",
-          color: "rgb(45, 75, 134)",
-        }}
-      >
-        <p>Instagram @pug_pantera</p>
-      </footer>
+    <div className="min-h-screen flex items-center justify-center">
+      <p>Redirecionando...</p>
     </div>
   );
-};
-
-export default HomePage;
+}
