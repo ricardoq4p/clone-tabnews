8da5d369a17104b13e73e0a8566d0f4ac9de554c
ricardoq4p <151384080+ricardoq4p@users.noreply.github.com>
2026-03-22 17:23:56 +0000

    fix: adiciona SessionProvider global

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
-import React from "react";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
-
-export default MyApp;
